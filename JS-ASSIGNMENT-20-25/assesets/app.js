b485627b7f26a2db808ac8720f5976a3ff25356d
Adeeba25 <128930569+Adeeba25@users.noreply.github.com>
2023-06-23 20:48:40 +0500

    Add files via upload

---
+++ JS-ASSIGNMENT-20-25/assesets/app.js
@@ -0,0 +1,50 @@
+// Q1//
+
+// let firstName= prompt("Enter your first name");
+// let lastName= prompt("Enter your last name");
+// let fullName= firstName +" "+ lastName;
+// alert("welcome " + fullName);
+
+// Q2//
+
+// let userMobile = prompt("enter your favourite phone model");
+// let mobileOfLength = userMobile.length;
+// document.write("I like " + userMobile + " mobile" + "<br>" + "Length of mobile : " + mobileOfLength);
+
+// Q3//
+
+// let letter = ("Pakistan");
+// let index = letter.indexOf("n");
+// document.write("String :" + letter + "<br>");
+// document.write("Index of 'n' : " + index);
+
+// Q4//
+
+// var word = "Hello World";
+// var index = word.lastIndexOf('l');
+// document.write("String :" + word + "<br>" + "Last index of 'l' " + index);
+
+// Q5//
+
+// let myNationality = "Pakistani";
+// let thirdIndex = myNationality[3];
+// document.write("String: " + myNationality + "<br>");
+// document.write("Character at index 3: " + thirdIndex);
+
+// Q6//
+
+// var userFirstName = prompt("Enter Your First Name").toUpperCase();
+// var userLastName = prompt("Enter Your Last Name").toUpperCase();
+// var result = userFirstName.concat(" " + userLastName);
+// alert("HELLO " + result);
+
+// Q7//
+
+// const city = ("Hydrabad");
+// const afterReplacement = city.replace("Hyder" , "Islam");
+// document.write("City: " + city + "<br>");
+// document.write("After Replacement: " + afterReplacementreplacement);
+
+var cityName1 = "Hyderabad";
+var replacing = cityName1.replace("Hyderabad","Islamabad")
+document.write("City :" + cityName1 + "<br>" + "After replacement: " + replacing); */
